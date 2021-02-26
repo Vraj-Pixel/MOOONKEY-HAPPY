9810b221ea20603699a741dc5874868d8107939c
Vraj-Pixel <72793006+Vraj-Pixel@users.noreply.github.com>
2021-02-26 17:43:51 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,129 @@
+var monkey, monkey_running
+var banana, bananaImage, obstacle, obstacleImage, food;
+var foodGroup, obstacleGroup;
+var score=0;
+var survivalTime=0;
+var backgroundImage;
+var bground;
+var gameOver,gameOverImage;
+
+function preload() {
+  monkey_running = loadAnimation("sprite_0.png", "sprite_1.png", "sprite_2.png", "sprite_3.png", "sprite_4.png", "sprite_5.png", "sprite_6.png", "sprite_7.png", "sprite_8.png");
+
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
+  backgroundImage=loadImage("jungle.jpg");
+  gameOverImage=loadImage("gameOver.png")
+}
+
+
+
+function setup() {
+  createCanvas(600, 500);
+  
+  bground=createSprite(300,300,600,500);
+  bground.addImage(backgroundImage);
+  bground.x = bground.width/2;
+  bground.scale=1.2
+  bground.velocityX = -2
+  
+  
+
+  monkey = createSprite(180, 445, 20, 20);
+  monkey.addAnimation("running", monkey_running);
+  monkey.scale = 0.1;
+ 
+
+  ground = createSprite(300, 500, 800, 10);
+  ground.shapeColor = "Brown";
+  ground.x = ground.width/2;
+  ground.visible=false;
+
+  gameOver=createSprite(300,250);
+
+  foodGroup = createGroup();
+  obstacleGroup = createGroup(); 
+ 
+}
+
+
+function draw() {
+  background(255);
+
+  if (keyDown("space")) {
+    monkey.velocityY = -10;
+  }
+    console.log(bground.x);
+    if(bground.x<200){
+     bground.x = bground.width/2;
+  }
+  monkey.velocityY = monkey.velocityY + 1.5;
+
+  monkey.collide(ground);
+
+  
+  
+   spawnFood();
+  spawnObstacles();
+  
+  
+  if(foodGroup.isTouching(monkey)){
+    score=score+2;
+    foodGroup.destroyEach();
+  }
+  
+  if(obstacleGroup.isTouching(monkey)){
+    monkey.velocityX=0;
+    bground.velocityX=0;
+    obstacleGroup.velocityX=0;
+    obstacleGroup.destroyEach(); 
+    gameOver.addImage(gameOverImage);
+  }
+  
+  
+  switch(score){ 
+    case 10: monkey.scale=0.12;
+                          break;
+   case 20: monkey.scale=0.14; 
+                          break; 
+   case 30: monkey.scale=0.16; 
+                          break;
+   case 40: monkey.scale=0.18;
+                         break; 
+   default: break; }
+  
+ 
+
+  drawSprites();
+  
+  
+  stroke("white");
+  textSize(20);
+  fill("white");
+  text("Score:"+score,500,20);
+}
+
+
+function spawnFood() {
+  if (frameCount % 80 === 0) {
+    
+    var food = createSprite(600, 450, 20, 40);
+    food.y = Math.round(random(120, 200));
+    food.addImage(bananaImage);
+    food.scale = 0.1;
+    food.velocityX = -5;
+    food.lifetime = 600;
+    foodGroup.add(food);
+  }
+ }
+function spawnObstacles() {
+  if (frameCount % 300 === 0) {
+    var obstacle = createSprite(600, 460, 20, 10);
+    obstacle.addImage(obstacleImage);
+    obstacle.scale = 0.2;
+    obstacle.velocityX = -8;
+    obstacle.lifetime = 600;
+    obstacleGroup.add(obstacle);
+  }
+}
+
